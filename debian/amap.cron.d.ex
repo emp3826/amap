6025315cf49195b2ae81fdaac9e7a2a037c6abf7
Raphaël Hertzog <hertzog@debian.org>
2012-04-02 13:47:57 +0000

    Delete all sample files that are not needed.
    I only kept the watch file.

--- debian/amap.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the amap package
-#
-0 4	* * *	root	[ -x /usr/bin/amap_maintenance ] && /usr/bin/amap_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(amap):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="amap" command="/usr/bin/amap"
